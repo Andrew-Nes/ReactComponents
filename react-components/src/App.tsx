7daf60033da29fbde524539590f9efd70b7af0b3
Andrei Niasmachny <Cejosoll@gmail.com>
2023-10-30 13:24:38 +0300

    feat: add search

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,8 +1,11 @@
 import './App.css';
 import { Component, ReactNode } from 'react';
+import Search from './Components/Search/Search';
 
 export default class App extends Component {
   render(): ReactNode {
-    return <div className="application"></div>;
+    return <div className="application">
+      <Search></Search>
+    </div>;
   }
 }
--- react-components/src/Components/Search/Search.tsx
+++ react-components/src/Components/Search/Search.tsx
@@ -1,7 +1,31 @@
-import { Component, ReactNode } from 'react';
+import { ChangeEvent, Component, ReactNode } from 'react';
 
 export default class Search extends Component {
+  searchWord: string = window.localStorage.getItem('searchWord') || '';
+  state = { word: this.searchWord,
+            results: []
+  };
+
+  onInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    this.setState({word: event.target.value});
+    window.localStorage.setItem('searchWord', event.target.value)
+  }
+
+  getData = async (searchWord: string) => {
+    const responce = await fetch(`https://swapi.dev/api/people/?search=${searchWord}&page=1`);
+    responce.json().then(data => {this.setState({results: data.results});
+  console.log(data.results)});
+  console.log('!!');
+  }
+
   render(): ReactNode {
-    return <div className="search-field"></div>;
+    return <div className="search-field">
+      <input 
+        type="text" 
+        className="search-input" 
+        value={this.state.word} 
+        onChange={this.onInputChange} />
+      <button className="search-button" onClick={() => this.getData(this.state.word)}>Search</button>
+    </div>;
   }
 }
