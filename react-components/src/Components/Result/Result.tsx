c3d97645b5bc64c0c91329d025d2a22dcb9cd43e
Andrei Niasmachny <Cejosoll@gmail.com>
2023-10-31 22:04:16 +0300

    fix: fix info display

--- react-components/src/Components/Result/Result.tsx
+++ react-components/src/Components/Result/Result.tsx
@@ -1,7 +1,16 @@
 import { Component, ReactNode } from 'react';
 
 interface searchResponseState {
-  searchResponse: object[];
+  searchResponse: {
+    name: string;
+    height: string;
+    mass: string;
+    hair_color: string;
+    skin_color: string;
+    eye_color: string;
+    birth_year: string;
+    gender: string;
+  }[];
 }
 export default class Result extends Component<searchResponseState> {
   render(): ReactNode {
@@ -9,7 +18,17 @@ export default class Result extends Component<searchResponseState> {
       return <div className="result-field">No results</div>;
     }
     const articles = this.props.searchResponse.map((el, index) => {
-      return <p key={index}>{JSON.stringify(el)}</p>;
+      return (
+        <p key={index}>
+          Name: {el.name} <br></br>
+          Height: {el.height} <br></br>
+          Weight: {el.mass} <br></br>
+          Hair: {el.hair_color} <br></br>
+          Skin: {el.skin_color} <br></br>
+          Eyes: {el.eye_color} <br></br>
+          Born: {el.birth_year} <br></br>
+        </p>
+      );
     });
     return <div className="result-field">{articles}</div>;
   }
